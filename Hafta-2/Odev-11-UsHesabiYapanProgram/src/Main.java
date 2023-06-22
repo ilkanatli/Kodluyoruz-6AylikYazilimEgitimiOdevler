c6b8ccae655bad07ffc7d806fded71b7cf39613c
İlkan Atlı <atliilkan@gmail.com>
2023-06-22 23:18:18 +0300

    Üs Hesabı Yapan Program

---
+++ Hafta-2/Odev-11-UsHesabiYapanProgram/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    static int power(int base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        } else if (base == 1) {
+            return 1;
+        }
+        return base * power(base, exponent - 1);
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Taban degerini giriniz: ");
+        int num1 = scan.nextInt();
+        System.out.print("Us degerini giriniz: ");
+        int num2 = scan.nextInt();
+        System.out.println(power(2,3));
+
+        // pow(2,3) => 2 * pow(2,2) -> 8
+        // pow(2,2) => 2 * pow(2,1) -> 4
+        // pow(2,1) => 2 * pow(2,0) -> 2
+        // pow(2,0) => 1
+    }
+}
---
+++ Hafta-2/readme.md
@@ -0,0 +1,9 @@
+# 2.Hafta Ödevler
+ 
+11. Ödev - Üs Hesabı Yapan Program
+12. Asal Sayı Bulan Program
+13. Desene Göre Metot Oluşturma
+14. Maaş Hesaplayıcı
+15. Dizideki Elemanları Sıralama
+16. Dizideki Elemanların Frekansı
+17. Matris Transpozunu Bulma
